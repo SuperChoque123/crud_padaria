08abacb89b933650792ad934291889bba659e0f0
SuperChoque123 <enzo_r_siqueira@estudante.sesisenai.org.br>
2025-11-07 08:56:00 -0300

    Add - database

---
+++ db.php
@@ -0,0 +1,10 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "root";
+$dbname = "crud_padaria";
+$conn = new mysqli($servername,$username,$password,$dbname);
+if ($conn->connect_error) {
+    die("Conexao Falhou: " . $conn -> connect_error);
+}
+?>
